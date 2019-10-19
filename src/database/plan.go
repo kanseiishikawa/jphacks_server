310c8abe9b136f981a87691281f8bf0b41d2db24
石川 幹晴 <kansei@kansei.local>
2019-10-19 15:33:59 +0900

    調整

--- src/database/plan.go
+++ src/database/plan.go
@@ -5,12 +5,30 @@ import (
 	"github.com/gocraft/dbr"
 )
 
-func Plan_Generate( id int, key string, db *dbr.Connection ) error {
+func Plan_Insert( id int, key string, db *dbr.Connection ) error {
 	_, err := db.Exec(
 		`INSERT INTO plan_data ( "id", "key", "answer_count" ) VALUES ( $1, $2, $3 )`,
 		id,
 		key,
 		0, )
+
+	return err
+}
+
+func Plan_Answer( sess *dbr.Session, key string ) ( int , error ) {
+	var count int
+
+	err := sess.QueryRow( `SELECT answer_count FROM plan_data WHERE key = $1`,
+		key, ).Scan( &count )
+
+	return count, err
+}
+
+func Plan_Answer_Update( sess *dbr.Session, key string, count int) error {
+
+	err := sess.QueryRow( `UPDATE plan_data SET answer_count = $1 WHERE key = $2`,
+		count,
+		key,)
 
 	return err
 }
