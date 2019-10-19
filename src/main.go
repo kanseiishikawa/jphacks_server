a60519ccf797e01301667d25811248d24f658382
石川 幹晴 <kansei@kansei.local>
2019-10-19 16:13:08 +0900

    formのapi

--- src/main.go
+++ src/main.go
@@ -22,6 +22,7 @@ func main() {
 	http.HandleFunc( "/test/v0", api.Test_http )
 	http.HandleFunc( "/app/v0/login", api.Login( config_data, keys ) )
 	http.HandleFunc( "/app/v0/plan_generate", api.Plan_Generate( config_data, keys ) )
+	http.HandleFunc( "/app/v0/plan_form", api.Plan_Form( config_data, keys ) )
 	
 	err = http.ListenAndServe( ":80", nil )
 
