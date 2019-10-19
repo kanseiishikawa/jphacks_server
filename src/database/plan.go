1e4fe79382eaab74448dfbd293f2852f31439461
石川 幹晴 <kansei@kansei.local>
2019-10-19 13:51:13 +0900

    修正

--- src/database/plan.go
+++ src/database/plan.go
@@ -7,7 +7,7 @@ import (
 
 func Plan_Generate( id int, key string, db *dbr.Connection ) error {
 	_, err := db.Exec(
-		`INSERT INTO plan_data ( "id", "key", "answer_account" ) VALUES ( $1, $2, $3 )`,
+		`INSERT INTO plan_data ( "id", "key", "answer_count" ) VALUES ( $1, $2, $3 )`,
 		id,
 		key,
 		0, )
